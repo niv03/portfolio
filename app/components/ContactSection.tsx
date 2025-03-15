e581e50aac9f95929400d6edd984fe0ae615bf45
niv03 <nivit@ualberta.ca>
2025-03-15 17:50:52 -0600

    updated

--- app/components/ContactSection.tsx
+++ app/components/ContactSection.tsx
@@ -33,7 +33,7 @@ export default function ContactSection() {
 									/>
 									<path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
 								</svg>
-								<span>San Francisco, CA</span>
+								<span>Edmonton, AB, Canada</span>
 							</div>
 							<p className="text-green-500 mt-8 mb-2">$ contact --email</p>
 							<motion.a
